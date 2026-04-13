c3f90022dbb694e200404bc6265836d9eceea0ec
DanielCastro1010 <daniel.c4@puccampinas.edu.br>
2026-04-13 16:59:01 -0300

    Add files via upload

---
+++ Tabela de Candidatos.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO candidatos VALUES
+(1, 'Antonio Luiz Beline', 'PL', '22'),
+(2, 'João Inacio da Silva', 'PT', '13'),
+(3, 'Luiz Paganine de Castro', 'PSDB', '30'),
+(4, 'José Marchioretto', 'PSB', '42'),
+(5, 'Pedro de Oliveira', 'PSD', '55');
