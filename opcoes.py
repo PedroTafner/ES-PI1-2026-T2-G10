aab957a5d14c6eafda979803927c97169e16cca4
Arthur Paioli <arthur.cp1@puccampinas.edu.br>
2026-05-05 10:41:30 -0300

    Arrumando erros da Validação do CPF - 30min

--- opcoes.py
+++ opcoes.py
@@ -115,7 +115,7 @@ def opcao_resultadoVotacao(): #OPÇÃO RESULTADO DA VOTAÇÃO
                 print("Opção Inválida")
                 
 def validacaoCPF(cpf):
-    if len(cpf) != 11 or cpf == cpf[0] * 11:
+    if len(str(cpf)) != 11 or cpf == str(cpf)[0] * 11:
         return False
     stringCPF=str(cpf)
     DV1=0
@@ -167,27 +167,28 @@ def titulo_eleitor(NUMTIT):
     
 
     DVTresto3=str(DVTresto)
-        if DVTresto >= 10:
-            DVTresto4=str(DVTresto)
-            DVTresto3=DVTresto4[1]
+    if DVTresto >= 10:
+        DVTresto4=str(DVTresto)
+        DVTresto3=DVTresto4[1]
 
-        if DVTresto3 != stringNUMTIT[10]:
-            print("titulo errado")
-            return False
-            DVTresto3=str(DVTresto)
+    if DVTresto3 != stringNUMTIT[10]:
+        print("titulo errado")
+        return False
+        DVTresto3=str(DVTresto)
 
-        DVTresto6=str(DVTresto2)
-        if DVTresto2 >= 10:
-            DVTresto5=str(DVTresto2)
-            DVTresto6=DVTresto5[1]
-            
-        if str(DVTresto6) != stringNUMTIT[11]:
-            print("titulo errado 2")
-            return False
-        print("Titulo valido")
+    DVTresto6=str(DVTresto2)
+    if DVTresto2 >= 10:
+        DVTresto5=str(DVTresto2)
+        DVTresto6=DVTresto5[1]
         
+    if str(DVTresto6) != stringNUMTIT[11]:
+        print("titulo errado 2")
+        return False
+    print("Titulo valido")
+    
     num=(input("Digite Seu Titulo:"))
-    aprovacao=titulo_eleitor(num)
+    aprovacao =titulo_eleitor(num)
+    return aprovacao
 
 
 def opcao_cadastro(): #OPÇÃO CADASTRO
