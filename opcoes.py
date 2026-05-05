ca604552bb0429f6710a6a954f36f5c1e65dbcfc
Daniel de Castro <daniel.c4@puccampinas.edu.br>
2026-05-05 10:15:08 -0300

    continuação da validação do titulo de eleitor (1 hora)

--- opcoes.py
+++ opcoes.py
@@ -164,6 +164,30 @@ def titulo_eleitor(NUMTIT):
         digitoT+=1
     DVTint=int(DVT/11) 
     DVTresto2=DVT-DVTint*11 
+    
+
+    DVTresto3=str(DVTresto)
+        if DVTresto >= 10:
+            DVTresto4=str(DVTresto)
+            DVTresto3=DVTresto4[1]
+
+        if DVTresto3 != stringNUMTIT[10]:
+            print("titulo errado")
+            return False
+            DVTresto3=str(DVTresto)
+
+        DVTresto6=str(DVTresto2)
+        if DVTresto2 >= 10:
+            DVTresto5=str(DVTresto2)
+            DVTresto6=DVTresto5[1]
+            
+        if str(DVTresto6) != stringNUMTIT[11]:
+            print("titulo errado 2")
+            return False
+        print("Titulo valido")
+        
+    num=(input("Digite Seu Titulo:"))
+    aprovacao=titulo_eleitor(num)
 
 
 def opcao_cadastro(): #OPÇÃO CADASTRO
